71afd61c35931425929a07f1e6f51bb1dd2b46d0
gelonsoft <gelonsoft@gmail.com>
2025-06-17 13:38:19 +0300

    t

--- run_train.sh
+++ run_train.sh
@@ -4,4 +4,4 @@ git reset
 git pull
 docker stop tms-bot-train || true
 docker rm tms-bot-train || true
-docker run --name tms-bot-train -v $(pwd):/data terraforming-mars-bot:latest python3 ppo.py
+docker run --name tms-bot-train -v $(pwd):/data terraforming-mars-bot:latest -e SERVER_BASE_URL=${SERVER_BASE_URL} python3 ppo.py
