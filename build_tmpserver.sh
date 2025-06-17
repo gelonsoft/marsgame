72e72612e67bc9fcc5e7540868f0c2e6f5826c30
gelonsoft <gelonsoft@gmail.com>
2025-06-17 12:44:18 +0300

    Fix

--- build_tmpserver.sh
+++ build_tmpserver.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 docker stop tms || true
 docker rm tms || true
-docker build -t terraforming-mars-server:latest -f Dockerfile.tmserver .
-# run_tmserver.sh
-docker run -d -p 9976:9976 --name tms terraforming-mars-server:latest
+docker build -t terraforming-mars-server:latest -f Dockerfile.tmserver . &&  docker run -d -p 9976:9976 --name tms terraforming-mars-server:latest
 # cleanup_tmserver.sh
--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #docker build -t terraforming-mars-bot:latest -f Dockerfile.train .
+git reset
+git fetch
 docker stop tms-bot-train || true
 docker run --name tms-bot-train -v $(pwd):/data terraforming-mars-bot:latest python3 ppo.py
