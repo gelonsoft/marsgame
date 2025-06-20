1efb603423f74076838bd3e504499b5b5ed8b56b
gelonsoft <gelonsoft@gmail.com>
2025-06-20 12:09:52 +0300

    debug bad actions:

--- env.py
+++ env.py
@@ -266,7 +266,7 @@ class TerraformingMarsEnv(ParallelEnv):
                 print(f"Agent {agent} selected input: {player_input.get('type')}")
                 res=self.post_player_input(agent, player_input)
                 if res is None:
-                    logging.error(f"Failed to post player input for agent {agent} with input: \n{json.dumps(player_input, indent=2)}\n and waiting steps \n{json.dumps(self.player_states[agent].get('waitingSteps',{}), indent=2)}\n")
+                    logging.error(f"Failed to post player input for agent {agent} with input player_link={SERVER_BASE_URL}/player?id={self.agent_id_to_player_id[agent]}: \n{json.dumps(player_input, indent=2)}\n and waiting steps \n{json.dumps(self.player_states[agent].get('waitingSteps',{}), indent=2)}\n")
                     player_input=None
 
             self.rewards[agent] = 1.0 if player_input else -1.0
